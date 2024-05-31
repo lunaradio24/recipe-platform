98b6f194cba25f6fc41125684cf6901c96f47233
Changjun Yeo <lunaradio24@gmail.com>
2024-05-31 16:38:48 +0900

    Set: 기본세팅 재설정

--- src/app.js
+++ src/app.js
@@ -1,11 +1,21 @@
-import express from "express";
-import "dotenv/config";
-import { SERVER_PORT } from "./constants/env.contant.js";
+import 'dotenv/config';
+import express from 'express';
+import { SERVER_PORT } from './constants/env.constant.js';
+import { HTTP_STATUS } from './constants/http-status.constant.js';
+import { apiRouter } from './routers/index.js';
 
-const app = express.Router();
+const app = express();
 
-app.use("/", []);
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/health-check', (req, res) => {
+  return res.status(HTTP_STATUS.OK).send(`I'm healthy`);
+});
+
+app.use('/', apiRouter);
+// app.use(errorHandler);
 
 app.listen(SERVER_PORT, () => {
-  console.log(`서버가 ${SERVER_PORT}번 포트에서 열렸습니다.`);
+  console.log(`서버가 ${SERVER_PORT}번 포트에서 실행중입니다.`);
 });
