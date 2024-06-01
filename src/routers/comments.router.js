8e60a8e5a8938a3b94852f52bb4c82538618ec28
lys <projectys29@gmail.com>
2024-06-02 03:21:11 +0900

    댓글 조회 api 기초

--- src/routers/comments.router.js
+++ src/routers/comments.router.js
@@ -37,4 +37,41 @@ commentRouter.post('/:postId/comments', commentValidator, async (req, res, next)
   }
 });
 
+//댓글 조회 api
+commentRouter.get('/:postId/comments', async (req, res, next) => {
+  try {
+    const { postId } = req.params;
+
+    const post = await prisma.post.findFirst({ where: { postId: +postId } });
+    if (!post) {
+      return res
+        .status(HTTP_STATUS.NOT_FOUND)
+        .json({ status: HTTP_STATUS.NOT_FOUND, message: '존재하지 않는 게시글입니다.' });
+    }
+
+    let comments = await prisma.comment.findMany({
+      where: { postId: +postId },
+      include: {
+        user: true,
+      },
+    });
+
+    comments = comments.map((comment) => {
+      return {
+        postId: comment.postId,
+        commentId: comment.commentId,
+        username: comment.user.username,
+        profileImage: comment.user.profileImage,
+        content: comment.content,
+        createdAt: comment.createdAt,
+        updatedAt: comment.updatedAt,
+      };
+    });
+
+    return res.status(200).json({ message: `${postId}번 게시글 댓글.`, comments });
+  } catch (error) {
+    next(error);
+  }
+});
+
 export { commentRouter };
