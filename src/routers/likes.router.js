bf679669e32155cc469dd66e5c666bfc0541b778
Changjun Yeo <lunaradio24@gmail.com>
2024-06-02 17:17:23 +0900

    Update: 해당 게시글 및 댓글 존재하는지 확인하는 과정 추가 #9

--- src/routers/likes.router.js
+++ src/routers/likes.router.js
@@ -7,18 +7,20 @@ const likeRouter = express.Router();
 // 게시글에 좋아요/취소 API <<< TODO: AccessToken 인증 미들웨어 거쳐야함
 likeRouter.put('/:postId/likes', async (req, res, next) => {
   try {
-    const { userId } = req.user;
+    const userId = 1; //테스트용
+    // const { userId } = req.user;
     const { postId } = req.params;
 
-    // 해당 게시글의 작성자 id 가져오기
-    const { userId: authorId } = await prisma.post.findUnique({
-      where: {
-        postId: +postId,
-      },
+    // 해당 게시글 가져오기
+    const post = await prisma.post.findUnique({
+      where: { postId: +postId },
     });
 
+    // 해당 게시글이 존재하는지 확인
+    if (!post) throw new Error('해당 게시글이 존재하지 않습니다.');
+
     // 본인이 작성한 게시글인지 확인
-    if (userId === authorId) {
+    if (userId === post.authorId) {
       throw new Error('본인이 작성한 게시글에는 좋아요를 누를 수 없습니다.');
     }
 
@@ -67,11 +69,17 @@ likeRouter.get('/:postId/likes', async (req, res, next) => {
   try {
     const { postId } = req.params;
 
+    // 해당 게시글 가져오기
+    const post = await prisma.post.findUnique({
+      where: { postId: +postId },
+    });
+
+    // 해당 게시글이 존재하는지 확인
+    if (!post) throw new Error('해당 게시글이 존재하지 않습니다.');
+
     // likes 테이블에서 특정 posts의 좋아요 검색
     const likes = await prisma.postLike.findMany({
-      where: {
-        postId: +postId,
-      },
+      where: { postId: +postId },
     });
 
     // 반환 정보
@@ -93,15 +101,16 @@ likeRouter.put('/:postId/comments/:commentId/likes', async (req, res, next) => {
     const { userId } = req.user;
     const { commentId } = req.params;
 
-    // 해당 댓글의 작성자 id 가져오기
-    const { userId: commenterId } = await prisma.comment.findUnique({
-      where: {
-        commentId: +commentId,
-      },
+    // 해당 댓글 가져오기
+    const comment = await prisma.comment.findUnique({
+      where: { commentId: +commentId },
     });
 
+    // 해당 게시글이 존재하는지 확인
+    if (!comment) throw new Error('해당 댓글이 존재하지 않습니다.');
+
     // 본인이 작성한 댓글인지 확인
-    if (userId === commenterId) {
+    if (userId === comment.commenterId) {
       throw new Error('본인이 작성한 댓글에는 좋아요를 누를 수 없습니다.');
     }
 
@@ -150,11 +159,17 @@ likeRouter.get('/:postId/comments/:commentId/likes', async (req, res, next) => {
   try {
     const { commentId } = req.params;
 
+    // 해당 댓글 가져오기
+    const comment = await prisma.comment.findUnique({
+      where: { commentId: +commentId },
+    });
+
+    // 해당 게시글이 존재하는지 확인
+    if (!comment) throw new Error('해당 댓글이 존재하지 않습니다.');
+
     // likes 테이블에서 해당 댓글의 좋아요 검색
     const likes = await prisma.commentLike.findMany({
-      where: {
-        commentId: +commentId,
-      },
+      where: { commentId: +commentId },
     });
 
     // 반환 정보
