be49995a312f3a1572173d570314063b70123614
Changjun Yeo <lunaradio24@gmail.com>
2024-06-03 16:30:10 +0900

    Add: custom-error-constructor in utils folder, Fix: some small errors in likes.router.js, Add: created_at to each of Post and Comment schema model

---
+++ src/utils/custom-error.util.js
@@ -0,0 +1,11 @@
+// Customized Error
+class CustomError extends Error {
+  constructor(code, message) {
+    // 부모 클래스(Error)의 생성자 호출
+    super(message);
+    // 에러 객체의 속성 설정
+    this.code = code;
+  }
+}
+
+export default CustomError;
