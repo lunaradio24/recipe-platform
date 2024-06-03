f2040dcb1face9aff5257fba4e23f5fb66cb084d
JoYoungJinNode5 <jyj950406@naver.com>
2024-06-03 15:02:02 +0900

    perf:RestFull하게 리빌딩

--- src/routers/users.router.js
+++ src/routers/users.router.js
@@ -1,26 +1,20 @@
 import express from 'express';
 import { prisma } from '../utils/prisma.util.js';
-import { Prisma } from '@prisma/client';
 import { HTTP_STATUS } from '../constants/http-status.constant.js';
 
 const userRouter = express.Router();
 
-// 1. 요청 파라미터와 바디에서 email과 username을 전달받습니다.
-userRouter.get('/users', async (req, res, next) => {
+// 프로필 조회 API
+userRouter.get('/users/:email', async (req, res, next) => {
   try {
-    const { userId } = req.user;
-
-    // 고유한 이메일로 유저 가저오기
-    // const user = await prisma.user.findUnique({
-    //     where: { email: +email },
-    //   });
+    const { email } = req.params;
 
     //유저 이메일로 이름, 이메일, 프로필이미지, 팔로우 숫자, 소개글 가져오기
     const user = await prisma.user.findUnique({
       where: { email: +email },
       include: {
         followers: true,
-      },
+      }, //follower 테이블에 따라 변경 가능
     });
 
     // 해당 이메일을 가진 유저가 있는지 확인하기
@@ -29,25 +23,71 @@ userRouter.get('/users', async (req, res, next) => {
         status: HTTP_STATUS.NOT_FOUND,
         message: '존재하지 않는 회원입니다.',
       });
-    } else {
-      users = users.map((user) => {
-        return {
-          //프로필 가져오기
-          email: user.email,
-          username: user.username,
-          profileImage: user.profileImage,
-          introduction: user.introduction,
-          followCount: user.followCount,
-          createdAt: comment.createdAt,
-          updatedAt: comment.updatedAt,
-        };
-      });
     }
+
+    //프로필 가져오기
+    const userProfile = {
+      email: user.email,
+      username: user.username,
+      profileImage: user.profileImage,
+      introduction: user.introduction,
+      followerCount: user.followerCount, //or 'followers.length'
+      createdAt: comment.createdAt,
+      updatedAt: comment.updatedAt,
+    };
+
+    return res.status(HTTP_STATUS.OK).json({
+      status: HTTP_STATUS.OK,
+      message: `${email}의 프로필.`,
+      userProfile,
+    });
+
     //에러 처리 try,catch
   } catch (error) {
     next(error);
   }
 });
 
-userRouter.patch('/users', async (req, res, next) => {});
+// 프로필 수정 API
+userRouter.patch('/users/:email', async (req, res, next) => {
+  const { email } = req.params;
+  const { username, profileImage, introduction } = req.body;
+  try {
+    const { userId } = req.user;
+
+    const user = await prisma.user.findUnique({
+      where: { email: +email },
+    });
+
+    if (!user) {
+      return res.status(HTTP_STATUS.NOT_FOUND).json({
+        status: HTTP_STATUS.NOT_FOUND,
+        message: '존재하지 않는 회원입니다.',
+      });
+    }
+
+    if (userId !== user.userId) {
+      return res.status(HTTP_STATUS.FORBIDDEN).json({
+        status: HTTP_STATUS.FORBIDDEN,
+        message: '프로필 수정 권한이 없습니다.',
+      });
+    }
+
+    const updatedUser = await prisma.user.update({
+      where: { email: email },
+      data: {
+        userId: user.userId,
+        username: username || user.username,
+        profileImage: profileImage || user.profileImage,
+        introduction: introduction || user.introduction,
+      },
+    });
+
+    return res
+      .status(HTTP_STATUS.CREATED)
+      .json({ status: HTTP_STATUS.CREATED, message: '프로필을 수정했습니다', updatedUser });
+  } catch (error) {
+    next(error);
+  }
+});
 export { userRouter };
