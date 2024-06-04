54820e336a9ea565122e3f66d5890ead0ab7b3df
Changjun Yeo <lunaradio24@gmail.com>
2024-06-05 01:50:22 +0900

    Fix: postId에 속하지 않는 commentId에도 좋아요를 누를 수 있던 문제 수정

--- src/routers/likes.router.js
+++ src/routers/likes.router.js
@@ -14,14 +14,10 @@ likeRouter.put('/:postId/likes', authenticateToken, async (req, res, next) => {
     const { postId } = req.params;
 
     // 해당 게시글 가져오기
-    const post = await prisma.post.findFirst({
-      where: { postId: +postId },
-    });
+    const post = await prisma.post.findFirst({ where: { postId: +postId } });
 
     // 해당 게시글이 존재하는지 확인
-    if (!post) {
-      throw new CustomError(HTTP_STATUS.NOT_FOUND, '존재하지 않는 게시글입니다.');
-    }
+    if (!post) throw new CustomError(HTTP_STATUS.NOT_FOUND, '존재하지 않는 게시글입니다.');
 
     // 본인이 작성한 게시글인지 확인
     if (userId === post.authorId) {
@@ -30,10 +26,7 @@ likeRouter.put('/:postId/likes', authenticateToken, async (req, res, next) => {
 
     // post_likes 테이블에서 해당 유저가 해당 게시글에 남긴 좋아요를 검색
     const like = await prisma.postLike.findFirst({
-      where: {
-        userId: userId,
-        postId: +postId,
-      },
+      where: { userId: userId, postId: +postId },
     });
 
     // 없으면
@@ -42,10 +35,7 @@ likeRouter.put('/:postId/likes', authenticateToken, async (req, res, next) => {
         async (txn) => {
           // post_likes 테이블에 데이터 생성
           await txn.postLike.create({
-            data: {
-              userId: userId,
-              postId: +postId,
-            },
+            data: { userId: userId, postId: +postId },
           });
           // posts 테이블의 해당 post의 like_count를 +1
           await txn.post.update({
@@ -96,14 +86,10 @@ likeRouter.get('/:postId/likes', async (req, res, next) => {
     const post = await prisma.post.findFirst({ where: { postId: +postId } });
 
     // 해당 게시글이 존재하는지 확인
-    if (!post) {
-      throw new CustomError(HTTP_STATUS.NOT_FOUND, '존재하지 않는 게시글입니다.');
-    }
+    if (!post) throw new CustomError(HTTP_STATUS.NOT_FOUND, '존재하지 않는 게시글입니다.');
 
     // likes 테이블에서 특정 posts의 좋아요 검색
-    const likes = await prisma.postLike.findMany({
-      where: { postId: +postId },
-    });
+    const likes = await prisma.postLike.findMany({ where: { postId: +postId } });
 
     // 반환 정보
     return res.status(HTTP_STATUS.OK).json({
@@ -128,19 +114,15 @@ likeRouter.put('/:postId/comments/:commentId/likes', authenticateToken, async (r
     const post = await prisma.post.findFirst({ where: { postId: +postId } });
 
     // 해당 게시글이 존재하는지 확인
-    if (!post) {
-      throw new CustomError(HTTP_STATUS.NOT_FOUND, '존재하지 않는 게시글입니다.');
-    }
+    if (!post) throw new CustomError(HTTP_STATUS.NOT_FOUND, '존재하지 않는 게시글입니다.');
 
     // 해당 댓글 가져오기
     const comment = await prisma.comment.findFirst({
-      where: { commentId: +commentId },
+      where: { commentId: +commentId, postId: +postId },
     });
 
     // 해당 댓글이 존재하는지 확인
-    if (!comment) {
-      throw new CustomError(HTTP_STATUS.NOT_FOUND, '존재하지 않는 댓글입니다.');
-    }
+    if (!comment) throw new CustomError(HTTP_STATUS.NOT_FOUND, '존재하지 않는 댓글입니다.');
 
     // 본인이 작성한 댓글인지 확인
     if (userId === comment.commenterId) {
@@ -161,10 +143,7 @@ likeRouter.put('/:postId/comments/:commentId/likes', authenticateToken, async (r
         async (txn) => {
           // comment_likes 테이블에 데이터 생성
           await txn.commentLike.create({
-            data: {
-              userId: userId,
-              commentId: +commentId,
-            },
+            data: { userId: userId, commentId: +commentId },
           });
           // comments 테이블의 해당 comment의 like_count를 +1
           await txn.comment.update({
@@ -220,7 +199,9 @@ likeRouter.get('/:postId/comments/:commentId/likes', async (req, res, next) => {
     }
 
     // 해당 댓글 가져오기
-    const comment = await prisma.comment.findFirst({ where: { commentId: +commentId } });
+    const comment = await prisma.comment.findFirst({
+      where: { commentId: +commentId, postId: +postId },
+    });
 
     // 해당 댓글이 존재하는지 확인
     if (!comment) {
